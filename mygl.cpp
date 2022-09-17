1cbb738fdbeb533b7974cbb8a1c855ecf2944b5a
alefemoreira <delimaalefe@gmail.com>
2022-09-17 15:30:05 -0300

    set windown to presentation and upload some picture to report

--- mygl.cpp
+++ mygl.cpp
@@ -25,18 +25,55 @@ void drawTriangle(Pixel &v1, Pixel &v2, Pixel &v3, unsigned char r, unsigned cha
 void MyGlDraw(void) {
   Pixel origin (0, 0);
   Pixel destiny (511, 511);
-  drawLine(origin, destiny, 255, 0, 0, 255);
+  drawLine(origin, destiny, 50, 50, 50, 255);
 
   origin = Pixel(0, 511);
   destiny = Pixel(511, 0);
-  drawLine(origin, destiny, 0, 255, 0, 255);
+  drawLine(origin, destiny, 50, 50, 50, 255);
 
   origin = Pixel(0, 256);
   destiny = Pixel(511, 256);
-  drawLine(origin, destiny, 0, 0, 255, 255);
+  drawLine(origin, destiny, 50, 50, 50, 255);
 
   origin = Pixel(256, 0);
   destiny = Pixel(256, 511);
+  drawLine(origin, destiny, 50, 50, 50, 255);
+
+  origin = Pixel(0, 128);
+  destiny = Pixel(256, 256);
   drawLine(origin, destiny, 0, 255, 255, 255);
+
+  origin = Pixel(128, 0);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 255, 0, 255, 255);
+
+  origin = Pixel(512, 128);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 255, 0, 0, 255);
+
+  origin = Pixel(384, 0);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 0, 255, 0, 255);
+
+  origin = Pixel(0, 384);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 255, 255, 0, 255);
+
+  origin = Pixel(128, 512);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 255, 255, 128, 255);
+
+  origin = Pixel(512, 384);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 0, 0, 255, 255);
+
+  origin = Pixel(384, 512);
+  destiny = Pixel(256, 256);
+  drawLine(origin, destiny, 0, 0, 128, 255);
+
+  Pixel v1 (256 - 80, 256 + 30);
+  Pixel v2 (200 - 80, 200 + 30);
+  Pixel v3 (100 - 80, 300 + 30);
+  drawTriangle(v1, v2, v3, 226, 67, 41, 255);
 }
 
--- mygl.h
+++ mygl.h
@@ -10,7 +10,7 @@ void MyGlDraw(void);
 //*****************************************************************************
 void putPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void drawLine(Pixel &start, Pixel &end, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
-void drawTriange(Pixel &v1, Pixel &v2, Pixel &v3, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+void drawTriangle(Pixel &v1, Pixel &v2, Pixel &v3, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 //*****************************************************************************
 
 
